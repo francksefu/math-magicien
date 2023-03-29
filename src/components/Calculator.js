3bde9ec4cc98346bbe28c65b060e2ba26a6c1da3
francksefu <90141102+francksefu@users.noreply.github.com>
2023-03-30 01:49:48 +0200

    Correct linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,38 +2,38 @@ import '../App.css';
 
 function Calculator() {
   return (
-      <section>
-        <div className="tableau">0</div>
-        <div className="ligne">
-          <span>AC</span>
-          <span>+/-</span>
-          <span>%</span>
-          <span className="orange">+</span>
-        </div>
-        <div className="ligne">
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span className="orange">X</span>
-        </div>
-        <div className="ligne">
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span className="orange">-</span>
-        </div>
-        <div className="ligne">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span className="orange">+</span>
-        </div>
-        <div className="ligne">
-          <span className="deux">0</span>
-          <span className="dernier">.</span>
-          <span className="orange dernier">=</span>
-        </div>
-      </section>
+    <section>
+      <div className="tableau">0</div>
+      <div className="ligne">
+        <span>AC</span>
+        <span>+/-</span>
+        <span>%</span>
+        <span className="orange">+</span>
+      </div>
+      <div className="ligne">
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span className="orange">X</span>
+      </div>
+      <div className="ligne">
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span className="orange">-</span>
+      </div>
+      <div className="ligne">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span className="orange">+</span>
+      </div>
+      <div className="ligne">
+        <span className="deux">0</span>
+        <span className="dernier">.</span>
+        <span className="orange dernier">=</span>
+      </div>
+    </section>
   );
 }
 
