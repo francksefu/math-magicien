f6434483bd140fc3aaedc660f9cf2f73178de199
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-04 22:01:21 +0200

    Add feature of operation and make the button of the calc working

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,58 @@
+import { useState } from 'react';
 import '../App.css';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [result, setResult] = useState({});
+  const handleClick = (e) => {
+    setResult(calculate(result, e.target.textContent));
+  }
+  
   return (
     <section>
-      <div className="tableau">0</div>
+      <Affichage classons="tableau" afficheur={result.next == null? result.total:result.next} />
       <div className="ligne">
-        <span>AC</span>
-        <span>+/-</span>
-        <span>%</span>
-        <span className="orange">+</span>
+      <Span classons="aucun" name = "AC" onClick={handleClick} />
+      <Span classons="aucun" name = "+/-" onClick={handleClick} />
+      <Span classons="aucun" name = "%" onClick={handleClick} />
+      <Span classons="orange" name = "+" onClick={handleClick} />
       </div>
       <div className="ligne">
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
-        <span className="orange">X</span>
+        <Span classons="aucun" name = "7" onClick={handleClick} />
+        <Span classons="aucun" name = "8" onClick={handleClick} />
+        <Span classons="aucun" name = "9" onClick={handleClick} />
+        <Span classons="orange" name = "x" onClick={handleClick} />
       </div>
       <div className="ligne">
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
-        <span className="orange">-</span>
+      <Span classons="aucun" name = "4" onClick={handleClick} />
+      <Span classons="aucun" name = "5" onClick={handleClick} />
+        <Span classons="aucun" name = "6" onClick={handleClick} />
+        <Span classons="orange" name = "-" onClick={handleClick} />
       </div>
       <div className="ligne">
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span className="orange">+</span>
+        <Span classons="aucun" name = "1" onClick={handleClick} />
+        <Span classons="aucun" name = "2" onClick={handleClick} />
+        <Span classons="aucun" name = "3" onClick={handleClick} />
+        <Span classons="orange" name = "+" onClick={handleClick} />
       </div>
       <div className="ligne">
-        <span className="deux">0</span>
-        <span className="dernier">.</span>
-        <span className="orange dernier">=</span>
+      <Span classons="deux" name ="0" onClick={handleClick} />
+      <Span classons="dernier" name ="." onClick={handleClick} />
+      <Span classons="dernier orange" name ="=" onClick={handleClick} />
       </div>
     </section>
+  );
+}
+
+const Span = ({ classons, name, onClick }) => {
+  return(
+    <span className={classons} onClick={onClick}>{name}</span>
+  );
+}
+
+const Affichage = (props) => {
+  return(
+    <div className={props.classons}>{props.afficheur}</div>
   );
 }
 
