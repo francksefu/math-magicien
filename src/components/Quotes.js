8046f210b25060299baf8685597d837db8c721d7
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-07 18:11:46 +0200

    fix linter

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,40 +1,45 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from 'react';
 
 function Quotes() {
   const [data, setData] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [loading, setLoading] = useState(false);
- 
+
   useEffect(() => {
     const send = () => {
-        setLoading(true);
-        try{
-            fetch('https://api.api-ninjas.com/v1/quotes?category=money', {
+      setLoading(true);
+      try{
+        fetch('https://api.api-ninjas.com/v1/quotes?category=money', {
           method: 'GET',
-          headers: { 'X-Api-Key': 'tTySwiau1PsXJs5wvhf1dw==NK8bNzv70yBdLNYz'},
+          headers: { 'X-Api-Key': 'tTySwiau1PsXJs5wvhf1dw==NK8bNzv70yBdLNYz' },
           contentType: 'application/json',
         })
           .then((response) => response.json())
           .then((json) => setData(json));
-        } catch {
-            setHasError(true)
-        }
+      } catch {
+        setHasError(true)
+      }
         setLoading(false)
       };
     send();
-  },[setData, setLoading]);
+  }, [setData, setLoading]);
 
-  if(hasError) return <h2>Something went wrong!</h2>;
-  if(loading) return <h2> Loading ...</h2>;
+  if (hasError) return <h2>Something went wrong!</h2>;
+  if (loading) return <h2> Loading ...</h2>;
   return (
-    <div >{data.length?(
+    <div>
+      { data.length ? (
       <div className="quotation">
         <h2>Quotes</h2>
         <p>{data[0].quote}</p>
-        <p><small>author : {data[0].author}</small></p>
-        <p><small>category : {data[0].category}</small></p>
+        <p><small>
+          author : {data[0].author}
+        </small></p>
+        <p><small>
+          category : {data[0].category}
+        </small></p>
       </div>
-    ):(<p>Loading..</p>)}
+    ) : (<p>Loading..</p>)}
     </div>
   );
 }
