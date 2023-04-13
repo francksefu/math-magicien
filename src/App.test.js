4ceca727592a7d48618393abb76e20f188e894cf
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-13 18:15:16 +0200

    Testing part

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
+//import TestRenderer from 'react-test-renderer';
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  ;
+  
+  expect(render(<Calculator />)).toMatchSnapshot();
+  expect(render(<Home />)).toMatchSnapshot();
+  expect(render(<Quotes />)).toMatchSnapshot();
 });
+/*
+import Link from '../Link';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Link page="http://www.facebook.com">Facebook</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});*/
+
+/*
+it('renders correctly', () => {
+  const tree = TestRenderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});*/
