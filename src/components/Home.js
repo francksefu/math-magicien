c78742138f75d2265e6972826d948fd174b52bf3
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-13 02:46:59 +0200

    Correct linter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 function Home() {
   return (
     <div className="home">
-    <h2>Home Welcome</h2>
+      <h2>Home Welcome</h2>
       <p>
-          Hello everyone, this is just a page, for trying 
+        Hello everyone, this is just a page, for trying 
           Hello everyone, this is just a page, for trying 
           Dans cette leçon, nous nous sommes concentrés principalement sur les
           options CSS dont vous disposez dans l’ARC. Si vous voulez en savoir 
-          plus sur le soutien CSS de l’ARC et sur la façon de le personnaliser, lisez la documentation de l’ARC, en particulier la section Styles et actifs.
+          plus sur le soutien CSS de l’ARC et sur la façon de le personnaliser, lisez
+        la documentation de l’ARC, en particulier la section Styles et actifs.
         </p>
         <p>
-          Dans cette leçon, nous nous sommes concentrés principalement sur 
-          les options CSS dont vous disposez dans l’ARC. Si vous voulez en 
-          savoir plus sur le soutien CSS de l’ARC et sur la façon de le personnaliser, 
-          lisez la documentation de l’ARC, en particulier la section Styles et actifs.
+        Dans cette leçon, nous nous sommes concentrés principalement sur 
+        les options CSS dont vous disposez dans l’ARC. Si vous voulez en 
+        savoir plus sur le soutien CSS de l’ARC et sur la façon de le personnaliser, 
+        lisez la documentation de l’ARC, en particulier la section Styles et actifs.
         </p>
     </div>
     
