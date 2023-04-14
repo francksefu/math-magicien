25cb1136aa69d5d87a6c18f8adcd5aa5e342c926
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-14 08:56:51 +0200

    fix linter error

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import Calculator from './components/Calculator';
+import '@testing-library/jest-dom';
+import Calculator, { Span } from './components/Calculator';
 import Home from './components/Home';
 import Quotes from './components/Quotes';
-import { Span } from './components/Calculator';
 
 // import TestRenderer from 'react-test-renderer';
 test('renders learn react link', () => {
@@ -12,18 +11,18 @@ test('renders learn react link', () => {
   expect(render(<Quotes />)).toMatchSnapshot();
 });
 
-test('testing button 1',  () => {
+test('testing button 1', () => {
   const mockClick = jest.fn();
-    render(<Span classons="aucun" valeur="1" cliquons={mockClick} />);
-    const button = screen.getByRole('button', { valeur: "1" });
-    fireEvent.click(button);
-    expect(mockClick).toHaveBeenCalled();
+  render(<Span classons="aucun" valeur="1" cliquons={mockClick} />);
+  const button = screen.getByRole('button', { valeur: '1' });
+  fireEvent.click(button);
+  expect(mockClick).toHaveBeenCalled();
 });
 
-test('testing button AC',  () => {
+test('testing button AC', () => {
   const mockClick = jest.fn();
-    render(<Span classons="aucun" valeur="AC" cliquons={mockClick} />);
-    const button = screen.getByRole('button', { valeur: "AC" });
-    fireEvent.click(button);
-    expect(mockClick).toHaveBeenCalled();
+  render(<Span classons="aucun" valeur="AC" cliquons={mockClick} />);
+  const button = screen.getByRole('button', { valeur: 'AC' });
+  fireEvent.click(button);
+  expect(mockClick).toHaveBeenCalled();
 });
